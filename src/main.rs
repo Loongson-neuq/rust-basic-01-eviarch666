cb1105feb3048461b5d6aebb76d6ecbd4ab672b1
eviarch <eviarch666.com>
2024-11-02 20:10:06 +0800

    finish

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Guess the number!\nPlease input your guess.");
+    
+    loop {
+        let mut guess = String::new();
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+        let your_guess: u32 = guess.trim().parse().expect("Please type a number!");
+        if your_guess > secret_number {
+            println!("Your number is bigger than the answer.")
+        } else if your_guess < secret_number {
+            println!("Your number is smaller than the answer.")
+        }else {
+            println!("Correct!");
+            break;
+        }
+    }
 }
